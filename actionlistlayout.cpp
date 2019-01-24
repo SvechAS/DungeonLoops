375d8d1103868e8702fb49d93ed80c5774cdbeb2
CawaEast <777.777.1234@mail.ru>
2019-01-25 02:37:06 +0300

    moved most of actionlistlayout to it's class

---
+++ actionlistlayout.cpp
@@ -0,0 +1,6 @@
+#include "actionlistlayout.h"
+
+ActionListLayout::ActionListLayout()
+{
+
+}
---
+++ actionlistlayout.h
@@ -0,0 +1,12 @@
+#ifndef ACTIONLISTLAYOUT_H
+#define ACTIONLISTLAYOUT_H
+
+#include <QObject>
+
+class ActionListLayout
+{
+public:
+    ActionListLayout();
+};
+
+#endif // ACTIONLISTLAYOUT_H
